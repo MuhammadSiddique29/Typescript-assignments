925082a6f7a2a9630bb819b18dc7a38f4c41ba13
Muhammad Siddique <159746902+MuhammadSiddique29@users.noreply.github.com>
2024-03-06 07:56:20 +0500

    Add files via upload

---
+++ typescript/ass_34/main.ts
@@ -0,0 +1,11 @@
+let myPizza: string[] = ["Cheeze Pizza", "Pepperoni Pizza", "Vegeterian Pizza"]
+
+for(let pizza of myPizza){
+    console.log(pizza)
+}
+
+for(let pizza of myPizza){
+    console.log(`I like ${pizza}`)
+}
+
+console.log("\nI really love pizza!")
---
+++ typescript/ass_35/main.ts
@@ -0,0 +1,11 @@
+let myAnimals: string[] = ["cat", "dog", "goat"]
+
+for(let animal of myAnimals){
+    console.log(animal)
+}
+
+for(let animal of myAnimals){
+    console.log(`A ${animal} would make a great pet.`)
+}
+
+console.log(`Any of these animals such as ${myAnimals[0]}, ${myAnimals[1]} and ${myAnimals[2]} would make a great pet!`)
